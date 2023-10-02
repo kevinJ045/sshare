1804180a935a19fb5e0e0ef64de0cd4d7484dc0f
makano <makanobush@gmail.com>
2023-10-02 21:15:22 +0300

    by id

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
-const screenshot = require('screenshot-desktop')
 
 
 const app = express();
@@ -12,32 +11,19 @@ const io = socketIo(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    let close, timeout;
     console.log('Client connected');
 
-    function sendData(){
-        screenshot({format: 'png'}).then((img) => {
-            socket.emit('data', img.toString('base64'));
-            if(!close) timeout = setTimeout(sendData, 16);
-        });
-    }
-
-    socket.on('screenshot', ({ imageData }) =>{
-        socket.broadcast.emit('data:ss', socket.id, imageData);
-    });
-
-    socket.on('start-stream', () => {
-        // Start screen capture and send frames to the client
-        sendData();
+    socket.on('screenshot', ({ room, imageData }) =>{
+        socket.broadcast.emit('data:'+room, imageData);
     });
 
     socket.on('disconnect', () => {
-        close = true;
-        clearTimeout(timeout);
         console.log('Client disconnected');
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log('Server is listening on port '+port);
 });
--- index.py
+++ index.py
@@ -2,37 +2,43 @@ import base64
 import socketio
 import mss
 import time
+import sys
 
 # Initialize the Socket.IO client
 sio = socketio.Client()
+room = 'home'
 
-# Connect to the Socket.IO server
+if len(sys.argv) > 1:
+	room = sys.argv[1]
+
+def send_screenshot():
+	try:
+		with mss.mss() as sct:
+			screenshot = sct.shot(output='screenshot.png')
+
+		with open('screenshot.png', 'rb') as image_file:
+			image_data = image_file.read()
+			base64_image = base64.b64encode(image_data).decode('utf-8')
+
+		sio.emit('screenshot', {'room': room, 'imageData': base64_image})
+		time.sleep(0.016)
+		send_screenshot()
+	except Exception as e:
+		print('Error')
+		time.sleep(0.016)
+		send_screenshot()
+
 @sio.event
 def connect():
-    print('Connected to the server')
+	print('Connected to the server')
+	send_screenshot()
 
 @sio.event
 def disconnect():
-    print('Disconnected from the server')
+  print('Disconnected from the server')
 
 sio.connect('http://localhost:3000')
 
+print('Room '+room)
 
-# Function to capture and send screenshot data
-def send_screenshot():
-    with mss.mss() as sct:
-        screenshot = sct.shot(output='screenshot.png')
-
-    with open('screenshot.png', 'rb') as image_file:
-        image_data = image_file.read()
-        base64_image = base64.b64encode(image_data).decode('utf-8')
-
-    sio.emit('screenshot', {'imageData': base64_image})
-
-# Continuously send screenshots every 16 ms
-while True:
-    send_screenshot()
-    time.sleep(0.016)  # 16 milliseconds
-
-# Run the Socket.IO client
 sio.wait()
